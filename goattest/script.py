f0dbe16339a417dded336bb896b487d632002477
AlexP92 <51406031+AlexP92@users.noreply.github.com>
2019-12-28 00:09:11 +0200

    Model for list Items and associated migration

--- goattest/script.py
+++ goattest/script.py
@@ -12,6 +12,10 @@ class VisitorTest(unittest.TestCase):
     def tearDown(self):
        self.browser.close()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('add_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_starting(self):
 
@@ -19,22 +23,26 @@ class VisitorTest(unittest.TestCase):
         self.assertIn( "ToDo",self.browser.title)
         header=self.browser.find_element_by_tag_name('h1')
         self.assertIn("To Do",header.text)
-        inputbox =self.browser.find_element_by_id("add_new_item")
+        inputbox =self.browser.find_element_by_id("id_new_item")
         self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to do item')
-        inputbox.send_keys("Buy peacock")
+        inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table=self.browser.find_element_by_id('list_table')
-        rows=table.find_elements_by_tag_name('tr')
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        self.assertTrue(
-            any(row.text == '1: Buy peacock' for row in rows),
-            "New to-do item did not appear in table"
-        )
-        # inputbox = self.browser.find_element_by_id("add_new_item")
-        # inputbox.send_keys("Fly peacock")
-        # inputbox.send_keys(Keys.ENTER)
-        # self.assertIn("1: Buy peacock", [row.text for row in rows])
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+        # Edith wonders whether the site will remember her list. Then she sees
         self.fail('finish')
 
 if __name__=="__main__":
